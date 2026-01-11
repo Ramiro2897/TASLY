33e82aa2cb45c941ca11e8eb73d732b7859f6e67
Ramiro <ramirodjaltor2016@gmail.com>
2026-01-11 10:23:29 -0500

    Animación de carga mejorada

--- src/components/BootScreen.tsx
+++ src/components/BootScreen.tsx
@@ -3,12 +3,12 @@ import styles from '../styles/home.module.css';
 const BootScreen = () => {
   return (
     <div className={styles.bootScreen}>
-      <p>Preparando tu espacio…</p>
-      <small>Esto puede tardar unos segundos</small>
+      <div className={styles.loader}>
+        <span></span>
+        <span></span>
+      </div>
     </div>
   );
 };
 
 export default BootScreen;
- 
- 
